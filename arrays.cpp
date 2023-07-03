fdbbe4d07a7b6cf73d994d74b846135576de653c
Anushka Dubey <anushkadubey388@gmail.com>
2023-07-03 08:00:16 +0530

    Add files via upload

--- arrays.cpp
+++ arrays.cpp
@@ -36,82 +36,158 @@ using namespace std;
 //             }
 //         }
 //     }
-    
-    
-    // optimal solution
-    class Solution
-    {
-    public:
-        void setZeroes(vector<vector<int>> &matrix)
-        {
 
-            int n = matrix.size();
-            int m = matrix[0].size();
+// optimal solution
+/* class Solution
+ {
+ public:
+     void setZeroes(vector<vector<int>> &matrix)
+     {
 
-            // Declaring two boolean variables firstColZero and firstRowZero.
-            bool firstColZero = false, firstRowZero = false;
+         int n = matrix.size();
+         int m = matrix[0].size();
 
-            // Setting firstColZero to true if any element in first column is zero.
-            for (int i = 0; i < n; i++)
-            {
-                if (matrix[i][0] == 0)
-                {
-                    firstColZero = true;
-                    break;
-                }
-            }
+         // Declaring two boolean variables firstColZero and firstRowZero.
+         bool firstColZero = false, firstRowZero = false;
 
-            // Setting firstRowZero to true if any element in first row is zero.
-            for (int j = 0; j < m; j++)
-            {
-                if (matrix[0][j] == 0)
-                {
-                    firstRowZero = true;
-                    break;
-                }
-            }
+         // Setting firstColZero to true if any element in first column is zero.
+         for (int i = 0; i < n; i++)
+         {
+             if (matrix[i][0] == 0)
+             {
+                 firstColZero = true;
+                 break;
+             }
+         }
 
-            // Setting values for each row and column to be zero or not.
-            for (int i = 1; i < n; i++)
-            {
-                for (int j = 1; j < m; j++)
-                {
-                    if (matrix[i][j] == 0)
-                    {
-                        matrix[i][0] = matrix[0][j] = 0;
-                    }
-                }
-            }
+         // Setting firstRowZero to true if any element in first row is zero.
+         for (int j = 0; j < m; j++)
+         {
+             if (matrix[0][j] == 0)
+             {
+                 firstRowZero = true;
+                 break;
+             }
+         }
 
-            // If any element of first row is zero then that complete
-            // column should be zero and vice-versa.
-            for (int i = 1; i < n; i++)
-            {
-                for (int j = 1; j < m; j++)
-                {
-                    if (matrix[i][0] == 0 || matrix[0][j] == 0)
-                    {
-                        matrix[i][j] = 0;
-                    }
-                }
-            }
+         // Setting values for each row and column to be zero or not.
+         for (int i = 1; i < n; i++)
+         {
+             for (int j = 1; j < m; j++)
+             {
+                 if (matrix[i][j] == 0)
+                 {
+                     matrix[i][0] = matrix[0][j] = 0;
+                 }
+             }
+         }
 
-            // Setting first row to zero if firstRowZero is true.
-            if (firstColZero)
-            {
-                for (int i = 0; i < n; i++)
-                {
-                    matrix[i][0] = 0;
-                }
-            }
+         // If any element of first row is zero then that complete
+         // column should be zero and vice-versa.
+         for (int i = 1; i < n; i++)
+         {
+             for (int j = 1; j < m; j++)
+             {
+                 if (matrix[i][0] == 0 || matrix[0][j] == 0)
+                 {
+                     matrix[i][j] = 0;
+                 }
+             }
+         }
 
-            // Setting first column to zero if firstColZero is true.
-            if (firstRowZero)
-            {
-                for (int j = 0; j < m; j++)
-                {
-                    matrix[0][j] = 0;
-                }
-            }
+         // Setting first row to zero if firstRowZero is true.
+         if (firstColZero)
+         {
+             for (int i = 0; i < n; i++)
+             {
+                 matrix[i][0] = 0;
+             }
+         }
+
+         // Setting first column to zero if firstColZero is true.
+         if (firstRowZero)
+         {
+             for (int j = 0; j < m; j++)
+             {
+                 matrix[0][j] = 0;
+             }
+         }
+     }
+ };*/
+
+// pascal triangle
+/*vector<vector<long long int>> printPascal(int n)
+{
+  // Write your code here.
+  vector<vector<long long int>> res;
+  for(int i=0; i<n; i++){
+    vector<long long int>p;
+    for(int j=0; j<=i; j++){
+      if(j==0 || j==i) p.push_back(1);
+      else{
+        p.push_back(res[i-1][j-1]+res[i-1][j]);
+      }
+    }
+    res.push_back(p);
+  }
+  return res;
+}*/
+
+// kadane's algorithm-->maximum subaaray sum
+/*long long maxSubarraySum(int arr[], int n)
+{
+    long long sum = 0,maxi = LONG_MIN;
+
+    for(int i=0; i<n; i++){
+        sum+=arr[i];
+        //every time we
+        // get inside loop we will add to the sum
+
+        if(sum > maxi){
+            maxi = sum;
         }
-    };
+
+        if(sum<0){
+            sum=0;//reinitialize sum
+            //  to 0 if it is going less than 0
+        }
+    }
+    if(maxi<0) maxi=0;
+    return maxi;
+
+}*/
+
+// sort 0,1,2
+/*int low=0, mid=0, high=n-1;
+//keep moving until we have unsorted section
+while(mid<=high){
+   if(arr[mid] == 0){
+      swap(arr[low], arr[mid]);
+      low++;
+      mid++;
+
+   }
+   else if(arr[mid] == 1){
+      mid++;
+   }
+   else{
+      swap(arr[mid], arr[high]);
+      high--;
+   }
+}*/
+
+// buuy sell stock
+int maximumProfit(vector<int> &prices)
+{
+
+    int mini = prices[0];
+    int maxProfit = 0;
+    int n = prices.size();
+    for (int i = 0; i < n; i++)
+    {
+        int cost = prices[i] - mini;
+        maxProfit = max(maxProfit, cost);
+        mini = min(mini, prices[i]);
+    }
+    return maxProfit;
+}
