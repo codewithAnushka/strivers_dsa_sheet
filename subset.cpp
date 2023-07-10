6056a8d9300d2ff1071a865255c0cebc5696e50c
Anushka Dubey <anushkadubey388@gmail.com>
2023-07-10 09:11:46 +0530

    Add files via upload

---
+++ subset.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+#include<vector>
+using namespace std;
+
+void func(int ind, int sum, vector<int> &arr, int n, vector<int>&sumSubset)
+{
+	if(ind == n){
+		sumSubset.push_back(sum);
+		return;
+	}
+	func(ind+1, sum+arr[ind], arr, n, sumSubset);
+	func(ind+1, sum, arr, n, sumSubset);
+}
+vector<int> subsetSum(vector<int> &arr){
+		int n=arr.size();
+		vector<int> sumSubset;
+		func(0,0,arr,n,sumSubset);
+		sort(sumSubset.begin(), sumSubset.end());
+		return sumSubset;
+}
